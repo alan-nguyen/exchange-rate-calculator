93791189e10cfa05d7c5ba0f49804d68adfd6b75
Alan Nguyen <tuan8101@gmail.com>
2021-01-15 18:07:18 -0700

    Add event listeners

--- script.js
+++ script.js
@@ -6,5 +6,13 @@ const amountEl_two = document.getElementById('amount-two');
 const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
 
+// Fetch exchange rates and update the DOM
 function calculate() {}
+
+// Event listeners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+
 calculate();
